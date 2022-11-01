79964f0db22f173a33bd1feb43d584a22d22ffd7
SeekingAspdelus <jz332@duke.edu>
2022-11-01 12:35:03 +0800

    player

--- player.py
+++ player.py
@@ -1,13 +1,18 @@
 '''
 LastEditors: SeekingAspdelus jz332@duke.edu
 Date: 2022-10-27 12:19:15
-LastEditTime: 2022-10-27 14:19:03
+LastEditTime: 2022-11-01 12:17:17
 FilePath: \AI_Game_Agent\player.py
 
-All the method in this file is used to control the player's movement and action.
+All the method in this file is used to control the player's action.
 method may be called in this file:
     get_behavior()
-    
+    get_name()
+    get_money()
+    get_color()
+    get_action()
+    invest()
+    status()
 '''
 
 
@@ -15,15 +20,15 @@ import pandas
 
 class Player:
     def __init__(self, name, money, color):
-        self.name = name
-        self.money = money
-        self.color = color
-        self.behavior = []
+        self.name = name # player's name 
+        self.money = money # in Peso int
+        self.color = color # player's color
+        self.behavior = [] #player's used behavior
 
     def __str__(self):
         return f'{self.name} has {self.money} Peso, his/her color is {self.color}'  
 
-    def add_behavior(self, behavior):
+    def add_behavior(self, behavior): 
         self.behavior.append(behavior)
 
     def get_behavior(self):
@@ -58,4 +63,7 @@ class Player:
     def profit(self, money):
         self.money += money
 
-    
+    def status(self):
+        print(f'You are {self.name}, your color is {self.color}, you have {self.money} Peso')
+        for i in self.behavior:
+            print(f'You have invested in {i.name} for this turn')
