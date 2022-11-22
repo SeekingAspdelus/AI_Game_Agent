c0d13505feee3aca4530c39afff9a54509ae3acd
yutong195 <renyutong360@gmail.com>
2022-11-22 22:41:42 +0800

    Update

--- agents.py
+++ agents.py
@@ -1,7 +1,7 @@
 '''
 Author: Tianle Zhu
 Date: 2022-11-20 17:04:47
-LastEditTime: 2022-11-22 22:34:21
+LastEditTime: 2022-11-22 22:40:14
 LastEditors: Please set LastEditors
 FilePath: \AI_Game_Agent\agents.py
 '''
@@ -80,28 +80,28 @@ class QlearningAgent(Player):
         ship_pos_mid = ship_pos_ls_sort[1]
         ship_pos_min = ship_pos_ls_sort[0]
 
-        if(action.type == "Ship"):
+        if(action.get_type() == "ship"):
             payback = action.get_payback()/len(action.get_investors())-action.get_cost()
-            reward = payback + self.set_factor*payback*(3.5*self.game.current_round+action.get_position()-13)
+            reward = payback + self.set_factor*payback*(3.5*(3-self.game.current_round)+action.get_position()-13)
 
-        elif((action.type == "Port")):
+        elif((action.get_type == "port")):
             payback = action.get_payback() - action.get_cost()
             if(action.name == "Port1"):
-                reward = payback + self.set_factor*payback*(3.5*self.game.current_round+ship_pos_max-13)
+                reward = payback + self.set_factor*payback*(3.5*(3-self.game.current_round)+ship_pos_max-13)
             elif(action.name == "Port2"):
-                reward = payback + self.set_factor*payback*(3.5*self.game.current_round+ship_pos_mid-13)
+                reward = payback + self.set_factor*payback*(3.5*(3-self.game.current_round)+ship_pos_mid-13)
             else:
-                reward = payback + self.set_factor*payback*(3.5*self.game.current_round+ship_pos_min-13)
+                reward = payback + self.set_factor*payback*(3.5*(3-self.game.current_round)+ship_pos_min-13)
                 
 
-        elif(action.type == "Shipyard"):
+        elif(action.get_type == "shipyard"):
             payback = action.get_payback() - action.get_cost()
             if(action.name == "Shipyard1"):
-                reward = payback - self.set_factor*payback*(3.5*self.game.current_round+ship_pos_min-13)
+                reward = payback - self.set_factor*payback*(3.5*(3-self.game.current_round)+ship_pos_min-13)
             elif(action.name == "Shipyard2"):
-                reward = payback - self.set_factor*payback*(3.5*self.game.current_round+ship_pos_mid-13)
+                reward = payback - self.set_factor*payback*(3.5*(3-self.game.current_round)+ship_pos_mid-13)
             else:
-                reward = payback - self.set_factor*payback*(3.5*self.game.current_round+ship_pos_max-13)
+                reward = payback - self.set_factor*payback*(3.5*(3-self.game.current_round)+ship_pos_max-13)
         else:
             reward = 0
         
