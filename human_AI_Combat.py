e8ba47b4e7a05e452d99850f15eda0db3bb78f84
SeekingAspdelus <jz332@duke.com>
2022-12-15 11:42:21 +0800

    SA

--- human_AI_Combat.py
+++ human_AI_Combat.py
@@ -8,23 +8,38 @@ FilePath: \AI_Game_Agent\human_AI_Combat.py
 import game
 import agents
 import play
+import argparse
 
-def main():
+def main(args):
     g = game.Game(True)
     player1 = play.Player("Player1", 30, None, g)
-    player2 = agents.QlearningAgent("Player2", 30, None, g)
-    player2.set_verbose(True)
-    player3 = agents.QlearningAgent("Player3", 30, None, g)
-    player3.set_verbose(True)
-    player2.loadQtable("qtable_normal.json")
-    player3.loadQtable("qtable_conservative.json")
+    if args.mode == 'Q_learning':
+        player2 = agents.QlearningAgent("Player2", 30, None, g)
+        player2.set_verbose(True)
+        player3 = agents.QlearningAgent("Player3", 30, None, g)
+        player3.set_verbose(True)
+        player2.loadQtable("qtable_"+ args.behavior1 +".json")
+        player3.loadQtable("qtable_"+ args.behavior2 +".json")
+    if args.mode == 'DQN':
+        player2 = agents.DQNAgent("Player2", 30, None, g)
+        player3 = agents.DQNAgent("Player3", 30, None, g)
+        player2.loadQtable("dqn_"+ args.behavior1 +".pth")
+        player3.loadQtable("dqn_"+ args.behavior2 +".pth")
+        player2.set_verbose(True)
+        player3.set_verbose(True)
     player_ls = [player1, player2, player3]
     g.add_player(player_ls)
     g.start()
     print("Player1's final money:", int(player_ls[0].money))
     print("Player2's final money:", int(player_ls[1].money))
     print("Player3's final money:", int(player_ls[2].money))
-    player2.saveQtable("qtable_normal.json")
-    player3.saveQtable("qtable_conservative.json")
 
-main()
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Allow human to play with AI in the Manila')
+    parser.add_argument('--mode', default='Q_learning', type=str, help='What method you want to use (Q_learning, DQN)')
+    parser.add_argument('--behavior1', default = 'normal', type=str, help='Mode of the agents (normal, conservative, aggressive)')
+    parser.add_argument('--behavior2', default = 'normal', type=str, help='Mode of the agents (normal, conservative, aggressive)')
+    args = parser.parse_args()
+    print(args)
+    main(args)   
